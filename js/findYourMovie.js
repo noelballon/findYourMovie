b9dbdca4f9b10a72ac8471201b62d22d9ff6cff1
noelballon <nballon@yahoo.com>
2016-06-29 07:41:57 -0700

    new JS file for movie API project

---
+++ js/findYourMovie.js
@@ -0,0 +1,35 @@
+ var xmlhttp;
+            
+     window.onload = function()
+     {
+       document.getElementById('btnGetInfo').addEventListener('click', getMovieInfo, false);
+     }
+
+     function getMovieInfo(e)
+       {
+           var title = document.getElementById('title').value;
+           var url = "http://www.omdbapi.com/?t=" + title + "&plot=full&r=json";
+           xmlhttp = new XMLHttpRequest();
+           xmlhttp.onreadystatechange = processData;
+           xmlhttp.open("GET", url, true);
+           xmlhttp.send()
+       }
+
+       function processData() {
+          if(xmlhttp.readyState==4 && xmlhttp.status== 200) {
+	         var movieJSON = xmlhttp.responseText;
+	         movieJSON = JSON.parse(movieJSON);
+	         var title= movieJSON.Title;
+	         var year = movieJSON.Year;
+	         var actors = movieJSON.Actors;
+	         var plot = movieJSON.Plot;
+	         var rating = movieJSON.Rated;
+	         var posterURL = movieJSON.Poster;
+	                        
+	         document.getElementById('poster').src = posterURL;
+	         document.getElementById('movieTitle').innerHTML = "<h1>" + title + " ["+ rating +"]</h1>";
+	         document.getElementById('movieYear').innerHTML += "<h3>" + year + "</h3>";
+	         document.getElementById('movieActors').innerHTML = "<p>" + actors + "</p>";
+	         document.getElementById('moviePlot').innerHTML += "<p>" + plot + "</p>";    		
+ 		 	}
+ 		}
